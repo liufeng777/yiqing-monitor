db2d055105f698806f9c5d7839b54d5b59b87b48
cdkchang <cdkchang@163.com>
2022-08-22 22:04:49 +0800

    style: 修改添加工程列表宽度

--- src/pages/Project/Detail.jsx
+++ src/pages/Project/Detail.jsx
@@ -58,207 +58,208 @@ export const ProjectDetail = (props) => {
         props.onCancel();
       }}
     >
-      <Form className="detail-form project-form" onFinish={(values) => {
-        props.onSubmit(detail);
-      }}
-      form={form}
-      initialValues={initialValues}
-      >
-        <Form.Item label="工程名" name="name"
-          rules={[{ required: true, message: '请输入工程名' }]}
+      <div className='project-form'>
+        <Form className="detail-form" onFinish={(values) => {
+          props.onSubmit(detail);
+        }}
+        form={form}
+        initialValues={initialValues}
         >
-          <Input maxLength={64} value={detail.name} onChange={(e) => {
-            setDetail({
-              ...detail,
-              name: e.target.value
-            })
-          }}
-          />
-        </Form.Item>
-        <Form.Item label="区域" name="area_code"
-          rules={[{ required: true, message: '请选择区域' }]}
-        >
-          <SelectArea visible={props.visible} area_code={detail.area_code} onChange={({code}) => {
-            setDetail({
-              ...detail,
-              area_code: code
-            })
-          }}
-          />
-        </Form.Item>
-        <Form.Item label="工程编码" name="project_code"
-          // rules={[{ 
-          //   validator(rule, value) {
-          //     const regx = /^(?![A-Z]+$)(?!\d+$)[0-9A-Z]{1,16}$/;
-          //     if (value && !regx.test(value)) {
-          //       return Promise.reject('由大写英文+数字组成, 最多16字符')
-          //     }
-          //     return Promise.resolve()
-          //   }
-          // }]}
-        >
-          <Input maxLength={16} value={detail.project_code} onChange={(e) => {
-            setDetail({
-              ...detail,
-              project_code: e.target.value.trim()
-            })
-          }}
-          />
-        </Form.Item>
-        <Form.Item label="状态" name="state">
-          <Select value={detail.state + ''} onChange={(val) => {
-            setDetail({
-              ...detail,
-              state: +val
-            })
-          }}
+          <Form.Item label="工程名" name="name"
+            rules={[{ required: true, message: '请输入工程名' }]}
           >
-            {
-              Object.keys(projectState).map((key) => {
-                return <Option value={key + ''} key={key}>{projectState[key]}</Option>
+            <Input maxLength={64} value={detail.name} onChange={(e) => {
+              setDetail({
+                ...detail,
+                name: e.target.value
               })
-            }
-          </Select>
-        </Form.Item>
-        <Form.Item label="图片" name="image_file">
-          <div className="upload-image">
-            {
-              imgUrl ? <img
-                alt=""
-                src={imgUrl}
-                style={{width: '100%', height: '100%'}}
-                       /> : <span onClick={throttle(1000, () => fileInput.current.click())}>上传图片</span>
-            }
-            {imgUrl && <section className="img-masker">
-              <i className="iconfont icon-xiugai" onClick={throttle(1000, () => fileInput.current.click())} />
-              <i className="iconfont icon-shanchu" onClick={throttle(1000, () => {
-                setImgUrl('');
-                setDetail({
-                  ...detail,
-                  image_file: '',
-                  image_path: ''
-                })
-              })}
-              />
-            </section>}
-          </div>
-        </Form.Item>
-        <Form.Item label="描述" name="comment">
-          <Input.TextArea maxLength={255} value={detail.comment} onChange={(e) => {
-            setDetail({
-              ...detail,
-              comment: e.target.value
-            })
-          }}
-          />
-        </Form.Item>
-        <Form.Item label="工程地址" name="address">
-          <Input maxLength={128} value={detail.address} onChange={(e) => {
-            setDetail({
-              ...detail,
-              address: e.target.value
-            })
-          }}
-          />
-        </Form.Item>
-        <Form.Item label="建筑面积" name="building_area">
-          <Input maxLength={32} value={detail.building_area} onChange={(e) => {
-            setDetail({
-              ...detail,
-              building_area: e.target.value
-            })
-          }}
-          />
-        </Form.Item>
-        <Form.Item label="幢数" name="building_number">
-          <InputNumber min={0} style={{width: 392}} value={detail.building_number} onChange={(val) => {
-            setDetail({
-              ...detail,
-              building_number: val
-            })
-          }}
-          />
-        </Form.Item>
-        <Form.Item label="建设单位" name="constructor">
-          <Input maxLength={45} value={detail.constructor} onChange={(e) => {
-            setDetail({
-              ...detail,
-              constructor: e.target.value
-            })
-          }}
-          />
-        </Form.Item>
-        <Form.Item label="建设单位联系人" name="constructor_user">
-          <Input maxLength={32} value={detail.constructor_user} onChange={(e) => {
-            setDetail({
-              ...detail,
-              constructor_user: e.target.value
-            })
-          }}
-          />
-        </Form.Item>
-        <Form.Item label="建设单位电话" name="constructor_phone">
-          <Input maxLength={16} value={detail.constructor_phone} onChange={(e) => {
-            setDetail({
-              ...detail,
-              constructor_phone: e.target.value
-            })
-          }}
-          />
-        </Form.Item>
-        <Form.Item label="施工单位" name="builder">
-          <Input maxLength={45} value={detail.builder} onChange={(e) => {
-            setDetail({
-              ...detail,
-              builder: e.target.value
-            })
-          }}
-          />
-        </Form.Item>
-        <Form.Item label="施工单位联系人" name="builder_user">
-          <Input maxLength={32} value={detail.builder_user} onChange={(e) => {
-            setDetail({
-              ...detail,
-              builder_user: e.target.value
-            })
-          }}
-          />
-        </Form.Item>
-        <Form.Item label="施工单位电话" name="builder_phone">
-          <Input maxLength={16} value={detail.builder_phone} onChange={(e) => {
-            setDetail({
-              ...detail,
-              builder_phone: e.target.value
-            })
-          }}
-          />
-        </Form.Item>
-        <Form.Item label="开工日期" name="build_timestamp">
-          <DateAndTime hideTime={true} value={detail.build_timestamp} onChange={(val) => {
-            setDetail({
-              ...detail,
-              build_timestamp: val
-            })
-          }}
-          />
-        </Form.Item>
-        <Form.Item>
-          <section className="form-btn">
-            <Button type="primary" htmlType="submit">
-              提交
-            </Button>
-            <Button htmlType="button" onClick={throttle(1000, () => {
-              setDetail(defaultDetail)
-              setImgUrl('');
-              props.onCancel();
-            })}
+            }}
+            />
+          </Form.Item>
+          <Form.Item label="区域" name="area_code"
+            rules={[{ required: true, message: '请选择区域' }]}
+          >
+            <SelectArea visible={props.visible} area_code={detail.area_code} onChange={({code}) => {
+              setDetail({
+                ...detail,
+                area_code: code
+              })
+            }}
+            />
+          </Form.Item>
+          <Form.Item label="工程编码" name="project_code"
+            // rules={[{ 
+            //   validator(rule, value) {
+            //     const regx = /^(?![A-Z]+$)(?!\d+$)[0-9A-Z]{1,16}$/;
+            //     if (value && !regx.test(value)) {
+            //       return Promise.reject('由大写英文+数字组成, 最多16字符')
+            //     }
+            //     return Promise.resolve()
+            //   }
+            // }]}
+          >
+            <Input maxLength={16} value={detail.project_code} onChange={(e) => {
+              setDetail({
+                ...detail,
+                project_code: e.target.value.trim()
+              })
+            }}
+            />
+          </Form.Item>
+          <Form.Item label="状态" name="state">
+            <Select value={detail.state + ''} onChange={(val) => {
+              setDetail({
+                ...detail,
+                state: +val
+              })
+            }}
             >
-              取消
-            </Button>
-          </section>
-        </Form.Item>
-      </Form>
-
+              {
+                Object.keys(projectState).map((key) => {
+                  return <Option value={key + ''} key={key}>{projectState[key]}</Option>
+                })
+              }
+            </Select>
+          </Form.Item>
+          <Form.Item label="图片" name="image_file">
+            <div className="upload-image">
+              {
+                imgUrl ? <img
+                  alt=""
+                  src={imgUrl}
+                  style={{width: '100%', height: '100%'}}
+                        /> : <span onClick={throttle(1000, () => fileInput.current.click())}>上传图片</span>
+              }
+              {imgUrl && <section className="img-masker">
+                <i className="iconfont icon-xiugai" onClick={throttle(1000, () => fileInput.current.click())} />
+                <i className="iconfont icon-shanchu" onClick={throttle(1000, () => {
+                  setImgUrl('');
+                  setDetail({
+                    ...detail,
+                    image_file: '',
+                    image_path: ''
+                  })
+                })}
+                />
+              </section>}
+            </div>
+          </Form.Item>
+          <Form.Item label="描述" name="comment">
+            <Input.TextArea maxLength={255} value={detail.comment} onChange={(e) => {
+              setDetail({
+                ...detail,
+                comment: e.target.value
+              })
+            }}
+            />
+          </Form.Item>
+          <Form.Item label="工程地址" name="address">
+            <Input maxLength={128} value={detail.address} onChange={(e) => {
+              setDetail({
+                ...detail,
+                address: e.target.value
+              })
+            }}
+            />
+          </Form.Item>
+          <Form.Item label="建筑面积" name="building_area">
+            <Input maxLength={32} value={detail.building_area} onChange={(e) => {
+              setDetail({
+                ...detail,
+                building_area: e.target.value
+              })
+            }}
+            />
+          </Form.Item>
+          <Form.Item label="幢数" name="building_number">
+            <InputNumber min={0} style={{width: 392}} value={detail.building_number} onChange={(val) => {
+              setDetail({
+                ...detail,
+                building_number: val
+              })
+            }}
+            />
+          </Form.Item>
+          <Form.Item label="建设单位" name="constructor">
+            <Input maxLength={45} value={detail.constructor} onChange={(e) => {
+              setDetail({
+                ...detail,
+                constructor: e.target.value
+              })
+            }}
+            />
+          </Form.Item>
+          <Form.Item label="建设单位联系人" name="constructor_user">
+            <Input maxLength={32} value={detail.constructor_user} onChange={(e) => {
+              setDetail({
+                ...detail,
+                constructor_user: e.target.value
+              })
+            }}
+            />
+          </Form.Item>
+          <Form.Item label="建设单位电话" name="constructor_phone">
+            <Input maxLength={16} value={detail.constructor_phone} onChange={(e) => {
+              setDetail({
+                ...detail,
+                constructor_phone: e.target.value
+              })
+            }}
+            />
+          </Form.Item>
+          <Form.Item label="施工单位" name="builder">
+            <Input maxLength={45} value={detail.builder} onChange={(e) => {
+              setDetail({
+                ...detail,
+                builder: e.target.value
+              })
+            }}
+            />
+          </Form.Item>
+          <Form.Item label="施工单位联系人" name="builder_user">
+            <Input maxLength={32} value={detail.builder_user} onChange={(e) => {
+              setDetail({
+                ...detail,
+                builder_user: e.target.value
+              })
+            }}
+            />
+          </Form.Item>
+          <Form.Item label="施工单位电话" name="builder_phone">
+            <Input maxLength={16} value={detail.builder_phone} onChange={(e) => {
+              setDetail({
+                ...detail,
+                builder_phone: e.target.value
+              })
+            }}
+            />
+          </Form.Item>
+          <Form.Item label="开工日期" name="build_timestamp">
+            <DateAndTime hideTime={true} value={detail.build_timestamp} onChange={(val) => {
+              setDetail({
+                ...detail,
+                build_timestamp: val
+              })
+            }}
+            />
+          </Form.Item>
+          <Form.Item>
+            <section className="form-btn">
+              <Button type="primary" htmlType="submit">
+                提交
+              </Button>
+              <Button htmlType="button" onClick={throttle(1000, () => {
+                setDetail(defaultDetail)
+                setImgUrl('');
+                props.onCancel();
+              })}
+              >
+                取消
+              </Button>
+            </section>
+          </Form.Item>
+        </Form>
+      </div>
       {/* 上传图片的input */}
       <input
         type="file"
